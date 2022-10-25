0edf5000b2c6f94309f32f3f622060e8284dce27
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-10-25 23:22:36 +0200

    Added css to MainPanel component

--- apps/web/src/components/MainPanel.jsx
+++ apps/web/src/components/MainPanel.jsx
@@ -7,18 +7,23 @@ import save from '../assets/icons/save.svg'
 
 export default function MainPanel() {
     return (
-        <div className="MainPanel">
+        <div className="main-panel">
            <div className='left-panel'>
-                <img src={add} alt="Add Button" />
-                <img src={search} alt="Search Button" />
+                {/* <img src={add} alt="Add Button" /> */}
+                <span className='add-icon'></span>
+                {/* <img src={search} alt="Search Button" /> */}
+                <span className='search-icon'></span>
            </div>
            <div className='center-panel'>
-                <img src={play} alt="Play Button" />
+                {/* <img src={play} alt="Play Button" /> */}
+                <span className='play-icon'></span>
                 <div className='dialogue-title'>Dialog Title</div>
            </div>
            <div className='right-panel'>
-                <img src={share} alt="Share Button" />
-                <img src={save} alt="Save Button" />
+                {/* <img src={share} alt="Share Button" /> */}
+                <span className='share-icon'></span>
+                {/* <img src={save} alt="Save Button" /> */}
+                <span className='save-icon'></span>
            </div>
         </div>
     )
