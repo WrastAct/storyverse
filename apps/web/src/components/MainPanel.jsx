034e30461b984c34e3bb90b38090283536c2d919
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-10-25 20:48:05 +0200

    Made MainPanel structure

--- apps/web/src/components/MainPanel.jsx
+++ apps/web/src/components/MainPanel.jsx
@@ -1,9 +1,25 @@
-
+import './MainPanel.css'
+import add from '../assets/icons/add.svg'
+import search from '../assets/icons/search.svg'
+import play from '../assets/icons/play.svg'
+import share from '../assets/icons/share.svg'
+import save from '../assets/icons/save.svg'
 
 export default function MainPanel() {
     return (
         <div className="MainPanel">
-            Hi! I am main panel
+           <div className='left-panel'>
+                <img src={add} alt="Add Button" />
+                <img src={search} alt="Search Button" />
+           </div>
+           <div className='center-panel'>
+                <img src={play} alt="Play Button" />
+                <div className='dialogue-title'>Dialog Title</div>
+           </div>
+           <div className='right-panel'>
+                <img src={share} alt="Share Button" />
+                <img src={save} alt="Save Button" />
+           </div>
         </div>
     )
 }
