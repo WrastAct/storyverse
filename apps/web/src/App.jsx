1f2f4ae892ea0f816b85b2d8eb30e190515a9c69
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-10-25 20:03:45 +0200

    Created MainPanel component

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import MainPanel from './components/MainPanel'
 
 function App() {
 
   return (
     <div className="App"> 
-        
+       <MainPanel />
     </div>
   )
 }
---
+++ apps/web/src/components/MainPanel.jsx
@@ -0,0 +1,9 @@
+
+
+export default function MainPanel() {
+    return (
+        <div className="MainPanel">
+            Hi! I am main panel
+        </div>
+    )
+}
