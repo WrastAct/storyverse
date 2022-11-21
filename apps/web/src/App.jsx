eff575fce844787e3623f6913005ad40b6cfd872
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-11-22 00:04:32 +0100

    Created basic routing

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css'
 import MainPanel from './components/MainPanel'
+import Authentication from './components/AuthPage'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
 
   return (
-    <div className="App"> 
-       <MainPanel />
-    </div>
+    <BrowserRouter>
+      <div className="App"> 
+        {/* <MainPanel /> */}
+        {/* <Authentication /> */}
+      </div>
+      <Routes>
+        <Route path="/login" element={<Authentication />} />
+        <Route path="/" element={<MainPanel />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
